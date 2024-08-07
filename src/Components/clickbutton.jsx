1786e088a64b1256f81851bc9f2943a2cc6f0683
John Kennedy <john,m,r,kennedy.jk@gmail.com>
2024-08-07 11:42:51 +0100

    toggle class name added for react button animation set timeout needed

--- src/Components/clickbutton.jsx
+++ src/Components/clickbutton.jsx
@@ -3,15 +3,21 @@ import { useState } from "react";
 export default function ClickButton() {
   const [count, SetCount] = useState(0);
   const [pulse, setPulse] = useState(false);
+  const ToggleClass = () => {
+    setPulse(!pulse);
+  };
+
   function handleclick() {
     SetCount(count + 1);
+    ToggleClass();
   }
+
   return (
     <>
       <p>You have {count} atoms</p>
       <div div className="clickbtn">
         <img
-          className="reactpulse"
+          className={pulse ? "reactpulse" : "react"}
           onClick={handleclick} //set timeout function to run animation
           src="../img/reactlogonbg.png"
           alt="React Logo"
